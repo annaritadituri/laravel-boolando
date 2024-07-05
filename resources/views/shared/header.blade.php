1187670129e5bead956ad10fff7c7bb7523c54fc
annaritadituri <annaritadituri@icloud.com>
2024-07-05 19:35:32 +0200

    [UPD] Added header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,3 +1,35 @@
 <header>
     
+    <div class="container">
+
+        <div class="row align-items-center text-center py-3">
+
+            <div class="col-3 left text-start ps-0">
+
+                <div class="section">
+                    <a href="" class="mx-2 ms-0">Donna</a>
+                    <a href="" class="mx-2">Uomo</a>
+                    <a href="" class="mx-2">Bambini</a>
+                </div>
+
+            </div>
+            <div class="col-6 center">
+
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="logo">
+                </div>
+
+            </div>
+            <div class="col-3 right text-end pe-0">
+
+                <button class="border-0"><i class="fas fa-user"></i></button>
+                <button class="border-0"><i class="fas fa-heart"></i></button>
+                <button class="border-0 pe-0"><i class="fas fa-cart-shopping"></i></button>
+
+            </div>
+
+        </div>
+
+    </div>
+    
 </header>
