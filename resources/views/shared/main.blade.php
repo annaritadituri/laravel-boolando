55bc7f32ba6508945c67e60d79679263b4baab3f
annaritadituri <annaritadituri@icloud.com>
2024-07-06 18:39:23 +0200

    [UPD] Added the products section

--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -1,10 +1,16 @@
 <main>
-    <ul>
-        @foreach ($products as $product)
-            <li>
-                @include('shared.product')
-            </li>
-        @endforeach
-    </ul>
+    <div class="list container text-center px-0 my-5">
+
+        <div class="row gy-5">
+
+            @foreach ($products as $product)
+                <div class="col-4">
+                    @include('shared.product')
+                </div>
+            @endforeach
+            
+        </div>
+
+    </div>
 
 </main>
--- resources/views/shared/product.blade.php
+++ resources/views/shared/product.blade.php
@@ -1,1 +1,46 @@
-{{ $product['name']}}
+<div class="product">
+
+    <img src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" alt="{{ $product['name'] }}">
+
+    <div class="on-image">
+
+        @for ($i = 0; $i < count($product['badges']); $i++)
+            @if ($product['badges'][$i]['type'] == 'discount')
+                <span class="discount-percent p-1 px-2 me-1">
+                    {{ $product['badges'][$i]['value'] }}
+                </span>
+            @endif
+        @endfor
+        
+        @for ($i = 0; $i < count($product['badges']); $i++)
+            @if ($product['badges'][$i]['type'] == 'tag')
+                <span class="label p-1 px-2">
+                    {{ $product['badges'][$i]['value'] }}
+                </span>
+            @endif
+        @endfor
+        
+    </div>
+
+    <p class="brand-name m-0 mt-1">{{ $product['brand'] }}</p>
+    <p class="product-name m-0 fw-bold">{{ $product['name'] }}</p>
+    <span class="sale">&euro;
+        @for ($i = 0; $i < count($product['badges']); $i++)
+            @if (count($product['badges']) == 2)
+                @if ($product['badges'][$i]['type'] == 'discount')
+                    {{ number_format($product['price'] + (intval($product['badges'][$i]['value']) / 100) * $product['price'], 2) }}
+                @endif
+            @else
+                @if ($product['badges'][$i]['type'] == 'discount')
+                    {{ number_format($product['price'] + (intval($product['badges'][$i]['value']) / 100) * $product['price'], 2) }}
+                @else
+                    {{ $product['price'] }}
+                @endif
+            @endif
+        @endfor
+
+        
+    </span>
+    <span class="price">&euro;{{ $product['price'] }}</span>
+
+</div>
